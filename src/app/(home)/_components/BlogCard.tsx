8c55d9245c3c938a00d61a2a6a6e132138389281
Tambuahc1ek <133321330+Tambuahc1ek@users.noreply.github.com>
2025-07-30 14:17:00 +0700

    feat/blog-list

--- src/app/(home)/_components/BlogCard.tsx
+++ src/app/(home)/_components/BlogCard.tsx
@@ -1,8 +1,8 @@
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { Blog } from "@/types/blog";
-import Image from "next/image";
-import Link from "next/link";
-import { FC } from "react";
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { Blog } from '@/types/blog';
+import Image from 'next/image';
+import Link from 'next/link';
+import { FC } from 'react';
 
 interface BlogCardProps {
   blog: Blog;
@@ -10,7 +10,7 @@ interface BlogCardProps {
 
 const BlogCard: FC<BlogCardProps> = ({ blog }) => {
   return (
-    <Link href={`/blogs/${blog.objectId}`}>
+    <Link href={`/blogs/${blog.slug}`}>
       <Card>
         <CardHeader>
           <div className="relative w-full h-[200px] rounded-lg overflow-hidden">
